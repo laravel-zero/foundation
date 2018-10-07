77144031cc9218095035444f5caf47227906a84a
Nuno Maduro <enunomaduro@gmail.com>
2018-10-07 17:46:44 +0200

    Adds version v5.7.8

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -28,7 +28,7 @@ class Application extends Container implements ApplicationContract
      *
      * @var string
      */
-    const VERSION = '5.7.6';
+    const VERSION = '5.7.8';
 
     /**
      * The base path for the Laravel installation.
