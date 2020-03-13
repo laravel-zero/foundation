74541d5114d10eec98ed6837c41961285fce907e
Owen Voke <development@voke.dev>
2020-03-13 11:14:07 +0000

    Update the Foundation to v7.1.1

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -32,7 +32,7 @@ class Application extends Container implements ApplicationContract, CachesConfig
      *
      * @var string
      */
-    const VERSION = '7.0.8';
+    const VERSION = '7.1.1';
 
     /**
      * The base path for the Laravel installation.
