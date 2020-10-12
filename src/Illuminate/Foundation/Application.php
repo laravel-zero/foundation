laravel-zero/foundation

commit cfd782104ced35ba28ccf0c69d7cc0bfe0a50a87

chore: update the Foundation to v8.9.0

src/Illuminate/Foundation/Application.php
@@ -32,7 +32,7 @@ class Application extends Container implements ApplicationContract, CachesConfig
      *
      * @var string
      */
-    const VERSION = '8.5.0';
+    const VERSION = '8.9.0';
 
     /**
      * The base path for the Laravel installation.

src/Illuminate/Foundation/Console/Kernel.php
@@ -223,7 +223,7 @@ class Kernel implements KernelContract
             $command = $namespace.str_replace(
                 ['/', '.php'],
                 ['\\', ''],
-                Str::after($command->getPathname(), app_path().DIRECTORY_SEPARATOR)
+                Str::after($command->getRealPath(), app_path().DIRECTORY_SEPARATOR)
             );
 
             if (is_subclass_of($command, Command::class) &&

src/Illuminate/Foundation/Console/ServeCommand.php
@@ -61,7 +61,9 @@ class ServeCommand extends Command
                 clearstatcache(false, $environmentFile);
             }
 
-            if ($hasEnvironment && filemtime($environmentFile) > $environmentLastModified) {
+            if (! $this->option('no-reload') &&
+                $hasEnvironment &&
+                filemtime($environmentFile) > $environmentLastModified) {
                 $environmentLastModified = filemtime($environmentFile);
 
                 $this->comment('Environment modified. Restarting server...');
@@ -93,6 +95,10 @@ class ServeCommand extends Command
     protected function startProcess()
     {
         $process = new Process($this->serverCommand(), null, collect($_ENV)->mapWithKeys(function ($value, $key) {
+            if ($this->option('no-reload')) {
+                return [$key => $value];
+            }
+
             return $key === 'APP_ENV'
                     ? [$key => $value]
                     : [$key => false];
@@ -164,6 +170,7 @@ class ServeCommand extends Command
             ['host', null, InputOption::VALUE_OPTIONAL, 'The host address to serve the application on', '127.0.0.1'],
             ['port', null, InputOption::VALUE_OPTIONAL, 'The port to serve the application on', Env::get('SERVER_PORT')],
             ['tries', null, InputOption::VALUE_OPTIONAL, 'The max number of ports to attempt to serve from', 10],
+            ['no-reload', null, InputOption::VALUE_NONE, 'Do not reload the development server on .env file changes'],
         ];
     }
 }

src/Illuminate/Foundation/Console/stubs/maintenance-mode.stub
@@ -41,7 +41,10 @@ if (isset($data['redirect']) && $_SERVER['REQUEST_URI'] !== $data['redirect']) {
 
 // Output the prerendered template...
 http_response_code($data['status'] ?? 503);
-header('Retry-After: '.$data['retry']);
+
+if (isset($data['retry'])) {
+    header('Retry-After: '.$data['retry']);
+}
 
 echo $data['template'];
 

src/Illuminate/Foundation/Console/stubs/view-component.stub
@@ -19,7 +19,7 @@ class DummyClass extends Component
     /**
      * Get the view / contents that represent the component.
      *
-     * @return \Illuminate\View\View|string
+     * @return \Illuminate\Contracts\View\View|string
      */
     public function render()
     {

src/Illuminate/Foundation/Providers/ArtisanServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Cache\Console\ClearCommand as CacheClearCommand;
 use Illuminate\Cache\Console\ForgetCommand as CacheForgetCommand;
 use Illuminate\Console\Scheduling\ScheduleFinishCommand;
 use Illuminate\Console\Scheduling\ScheduleRunCommand;
+use Illuminate\Console\Scheduling\ScheduleWorkCommand;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Database\Console\DumpCommand;
 use Illuminate\Database\Console\Factories\FactoryMakeCommand;
@@ -113,6 +114,7 @@ class ArtisanServiceProvider extends ServiceProvider implements DeferrableProvid
         'Seed' => 'command.seed',
         'ScheduleFinish' => ScheduleFinishCommand::class,
         'ScheduleRun' => ScheduleRunCommand::class,
+        'ScheduleWork' => ScheduleWorkCommand::class,
         'StorageLink' => 'command.storage.link',
         'Up' => 'command.up',
         'ViewCache' => 'command.view.cache',
@@ -912,6 +914,16 @@ class ArtisanServiceProvider extends ServiceProvider implements DeferrableProvid
     protected function registerScheduleRunCommand()
     {
         $this->app->singleton(ScheduleRunCommand::class);
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerScheduleWorkCommand()
+    {
+        $this->app->singleton(ScheduleWorkCommand::class);
     }
 
     /**

src/Illuminate/Foundation/helpers.php
@@ -673,7 +673,7 @@ if (! function_exists('response')) {
     /**
      * Return a new response from the application.
      *
-     * @param  \Illuminate\View\View|string|array|null  $content
+     * @param  \Illuminate\Contracts\View\View|string|array|null  $content
      * @param  int  $status
      * @param  array  $headers
      * @return \Illuminate\Http\Response|\Illuminate\Contracts\Routing\ResponseFactory
@@ -884,7 +884,7 @@ if (! function_exists('view')) {
      * @param  string|null  $view
      * @param  \Illuminate\Contracts\Support\Arrayable|array  $data
      * @param  array  $mergeData
-     * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
      */
     function view($view = null, $data = [], $mergeData = [])
     {

commit ca4e376824de3ffd1d558aedbfcaa1858de36c26

fix: revert change from getRealPath to getPathname
This reverts a breaking change to the autoloader that was introduced in cfd7821

Fixes https://github.com/laravel-zero/laravel-zero/issues/317

src/Illuminate/Foundation/Application.php
@@ -32,7 +32,7 @@ class Application extends Container implements ApplicationContract, CachesConfig
      *
      * @var string
      */
-    const VERSION = '8.9.0';
+    const VERSION = '8.9.1';
 
     /**
      * The base path for the Laravel installation.

src/Illuminate/Foundation/Console/Kernel.php
@@ -223,7 +223,7 @@ class Kernel implements KernelContract
             $command = $namespace.str_replace(
                 ['/', '.php'],
                 ['\\', ''],
-                Str::after($command->getRealPath(), app_path().DIRECTORY_SEPARATOR)
+                Str::after($command->getPathname(), app_path().DIRECTORY_SEPARATOR)
             );
 
             if (is_subclass_of($command, Command::class) &&