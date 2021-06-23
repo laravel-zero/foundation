f8a142aaa0e2d721b613e6cb876c2e12b43d9f9b
Owen Voke <development@voke.dev>
2021-06-23 15:22:11 +0100

    chore: update the Foundation to v8.48.1

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -32,7 +32,7 @@ class Application extends Container implements ApplicationContract, CachesConfig
      *
      * @var string
      */
-    const VERSION = '8.46.0';
+    const VERSION = '8.48.1';
 
     /**
      * Copied from HttpKernelInterface, which this class no longer extends.
