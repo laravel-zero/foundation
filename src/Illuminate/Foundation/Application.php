850097002009ef8c252b4aefa80f6c522df7ba8e
Owen Voke <development@voke.dev>
2021-12-20 17:13:32 +0000

    chore: update the Foundation to v8.76.1

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -32,7 +32,7 @@ class Application extends Container implements ApplicationContract, CachesConfig
      *
      * @var string
      */
-    const VERSION = '8.74.0';
+    const VERSION = '8.76.1';
 
     /**
      * Copied from HttpKernelInterface, which this class no longer extends.
@@ -254,7 +254,7 @@ class Application extends Container implements ApplicationContract, CachesConfig
      */
     public function afterLoadingEnvironment(Closure $callback)
     {
-        return $this->afterBootstrapping(
+        $this->afterBootstrapping(
             LoadEnvironmentVariables::class, $callback
         );
     }
