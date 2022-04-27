1f092d8074b4a6074e1eb0fb0a2f003087714bf9
Owen Voke <development@voke.dev>
2022-04-27 16:41:04 +0100

    chore: update the Foundation to v9.10.0

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -34,7 +34,7 @@ class Application extends Container implements ApplicationContract, CachesConfig
      *
      * @var string
      */
-    const VERSION = '9.9.0';
+    const VERSION = '9.10.0';
 
     /**
      * Copied from HttpKernelInterface, which this class no longer extends.
