695fe3b7ea7784f882fa3103daf120153233f492
Nuno Maduro <enunomaduro@gmail.com>
2024-04-18 18:11:23 +0100

    Removes usage of cookies

--- src/Illuminate/Foundation/Testing/Concerns/InteractsWithTestCaseLifecycle.php
+++ src/Illuminate/Foundation/Testing/Concerns/InteractsWithTestCaseLifecycle.php
@@ -4,7 +4,6 @@ namespace Illuminate\Foundation\Testing\Concerns;
 
 use Carbon\CarbonImmutable;
 use Illuminate\Console\Application as Artisan;
-use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Bootstrap\HandleExceptions;
 use Illuminate\Foundation\Bootstrap\RegisterProviders;
@@ -167,7 +166,6 @@ trait InteractsWithTestCaseLifecycle
         Component::forgetComponentsResolver();
         Component::forgetFactory();
         ConvertEmptyStringsToNull::flushState();
-        EncryptCookies::flushState();
         HandleExceptions::flushState();
         Once::flush();
         PreventRequestsDuringMaintenance::flushState();
