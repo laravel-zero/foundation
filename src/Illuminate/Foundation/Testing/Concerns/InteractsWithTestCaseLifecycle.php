bcc8d09a4e0705c128ca50c43291b8c91d43b44c
Mior Muhammad Zaki <crynobone@gmail.com>
2024-12-27 08:14:25 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Illuminate/Foundation/Testing/Concerns/InteractsWithTestCaseLifecycle.php
+++ src/Illuminate/Foundation/Testing/Concerns/InteractsWithTestCaseLifecycle.php
@@ -161,7 +161,7 @@ trait InteractsWithTestCaseLifecycle
         Component::forgetFactory();
         ConvertEmptyStringsToNull::flushState();
         HandleExceptions::forgetApp();
-            if (class_exists(Queue::class)) {
+        if (class_exists(Queue::class)) {
             Queue::createPayloadUsing(null);
         }
 
