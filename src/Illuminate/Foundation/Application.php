6ca71aa70897dbe1f456380fc458c9f8705d9bb2
Owen Voke <development@voke.dev>
2026-03-12 09:49:43 +0000

    chore: update the Foundation to v12.54.1

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -44,7 +44,7 @@ class Application extends Container implements ApplicationContract, CachesConfig
      *
      * @var string
      */
-    const VERSION = '12.53.0';
+    const VERSION = '12.54.1';
 
     /**
      * Copied from HttpKernelInterface, which this class no longer extends.
